e3c4120916747e72c78e7c1db9449a49d3c1e954
Pati <patrycja.starmach1993@gmail.com>
2022-12-12 10:18:56 +0100

    Debugging main function

--- credit-card.cpp
+++ credit-card.cpp
@@ -30,26 +30,23 @@ bool verifystatus(const Customer &cust)
     return false;
    }
    return true;
-    //TODo: Alle Karten muesssen gueltig (valid)sein.
-
 }
 
 void withdrawMoney(Customer &cust,const int amnt )
 {
     if (verifystatus(cust)==false){
         std::cerr<<"Karte ist nicht guelitig!"<<std::endl;
-    }else if(cust.mCustomer==CustomerState::Standard && cust.mCreditCard.mBalance<amnt){
+    }else if(cust.mCustomer==CustomerState::Standard && cust.mCreditCard.mBalance<amnt){   //Premimum Customers are allowed to go in minus
     std::cerr<<"Du hast nicht genugend Geld auf dem Konto, um die Transaktion durchzuehren."<<std::endl;
     }else{
         cust.mCreditCard.mBalance-=amnt;
     }
-    //TODO: Pruefen ob Kunde uebrehaupt Geld abheben darf, Fehler melden, falls nicht
-    //TODO:Premium-Kunden duerften sich weiterhin verschulden, standardkunden nicht.
-    //TODO: Dann Geld abheben
+
 }
 
 void showBalance(const CreditCard &card)
 {
+    std::cout<<"Du hast noch zur Verfuegung: "<<card.mBalance<<std::endl;
     //TODO:Anlegen wieviel Geld noch auf der Kreditkarte ist
 }
 
@@ -61,16 +58,12 @@ int main()
     peter.mCustomer=CustomerState::Premium;
     John.mCustomer=CustomerState::Standard;
 
-    std::cout<<"John als Standard Kunde hat zur Verfuegung: "<<John.mCreditCard.mBalance<<std::endl;
-    std::cout<<"Peter als Premium Kunde hat zur Verfuegung: "<<peter.mCreditCard.mBalance<<std::endl;
-
-    //TODO: Zeige hier an, wieviel Geld Sie zur Verfuegunghaben
+    std::cout<<"John: ";
+    showBalance(John.mCreditCard);
+    std::cout<<"Peter: ";
+    showBalance(peter.mCreditCard); // showing Balance befor withdrawal
 
     std::cout<<"Each is going to withdraw some Money: First50, then 100, and finally 200."<<std::endl;
-
-    if(verifystatus(peter)==true){
-
-    }
 
     withdrawMoney(John,50);
     withdrawMoney(peter,50);
@@ -81,12 +74,10 @@ int main()
     withdrawMoney(John,200);
     withdrawMoney(peter,200);
 
-    std::cout<<"Aktueller Kontostand fuer John: "<<John.mCreditCard.mBalance<<std::endl;
-    std::cout<<"Aktueller Kontostand fuer Peter: "<<peter.mCreditCard.mBalance<<std::endl;
-
-    //TODO:Zeige hier an, wie viel Geld beide jetzt zur Verfuegung habene
-
-
+    std::cout<<"John: ";            //showing Balance after withdrawal
+    showBalance(John.mCreditCard);
+    std::cout<<"peter: ";
+    showBalance(peter.mCreditCard);
 
 
     return 0;
